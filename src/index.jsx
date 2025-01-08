e62636d87ff167013105aa42890960bf86fd3f70
Kelvin Poon <panjiawen@vikadata.com>
2025-01-08 17:36:05 +0800

    initial project

---
+++ src/index.jsx
@@ -0,0 +1,254 @@
+import React, { useState, useEffect, useMemo } from "react";
+import {
+  initializeWidget,
+  useCloudStorage,
+  useRecords,
+  useFields,
+  useField,
+  FieldType,
+} from "@apitable/widget-sdk";
+import { Typography, Button } from "@apitable/components";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@apitable/icons";
+import { QRCodeSVG } from "qrcode.react";
+import { Setting } from "./setting";
+import style from "./index.css";
+
+export const VideoPlaylistPlayer = () => {
+  const [currentIndex, setCurrentIndex] = useState(0); // 当前播放的视频索引
+
+  const [viewId] = useCloudStorage("selectViewId", undefined);
+  const [videoFieldId] = useCloudStorage("videoFieldId", "");
+  const [showExtraInfo] = useCloudStorage("showExtraInfo", false);
+  const [extraInfoWidth] = useCloudStorage("extraInfoWidth", "20");
+  const [carouselMode] = useCloudStorage("carouselMode", "list");
+  const [playListMode] = useCloudStorage("playListMode", "all");
+
+  const [backgroundColor] = useCloudStorage(
+    "backgroundColor",
+    "var(--defaultBg)"
+  );
+
+  const [playList, setPlayList] = useState([]);
+
+  const fields = useFields(viewId);
+  const records = useRecords(viewId, { filter: {} });
+  const videoField = useField(videoFieldId);
+
+  // console.log({ records, viewId, currentIndex });
+
+  useEffect(() => {
+    if (records.length > 0 && currentIndex >= records.length) {
+      setCurrentIndex(0); // 如果索引超出数组长度，重置到第一个视频
+    }
+    // console.log("useEffect changed", { currentIndex, records });
+  }, [viewId, records, currentIndex]);
+
+  useEffect(() => {
+    if (carouselMode == "single") {
+      setCurrentIndex(0);
+    }
+  }, [carouselMode]);
+
+  useMemo(() => {
+    let videos = [];
+    if (videoFieldId !== "") {
+      records.forEach((record) => {
+        let url = "";
+        if (videoField.type === FieldType.URL) {
+          url = record.getCellValueString(videoFieldId);
+        } else if (videoField.type === FieldType.Attachment) {
+          const attachments = record.getCellValue(videoFieldId);
+          url = attachments[0].url;
+        }
+
+        url && videos.push(url);
+      });
+    }
+
+    if (playListMode === "firstItem") {
+      videos = videos.slice(0, 1);
+    } else if (playListMode === "lastItem") {
+      videos = videos.slice(-1);
+    }
+
+    // Only update playList if videos array has changed
+    if (JSON.stringify(videos) !== JSON.stringify(playList)) {
+      setPlayList(videos);
+      console.log("playList", playList);
+    }
+  }, [records]);
+
+  const handleVideoEnd = (event) => {
+    if (carouselMode == "none") {
+      return;
+    }
+
+    if (carouselMode == "list") {
+      if (currentIndex < playList.length - 1) {
+        setCurrentIndex((prevIndex) => prevIndex + 1); // 播放下一个视频
+      } else {
+        setCurrentIndex(0); // 所有视频播放结束后，重新开始
+      }
+    }
+    
+    event.target.play();
+  };
+
+  const handleNextVideo = () => {
+    if (currentIndex < playList.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1); // 播放下一个视频
+    } else {
+      setCurrentIndex(0); // 所有视频播放结束后，重新开始
+    }
+  };
+
+  const handlePrevVideo = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1); // 播放上一个视频
+    } else {
+      setCurrentIndex(playList.length - 1); // 播放第一个视频
+    }
+  };
+
+  const renderText = (fieldName, fieldText) => {
+    return fieldText ? (
+      <div className={style.mt6}>
+        <Typography variant="h3" className={style.extraInfoFieldName}>
+          {fieldName}
+        </Typography>
+        <Typography variant="body1" className={style.extraInfoFieldText}>
+          {fieldText || "（空）"}
+        </Typography>
+      </div>
+    ) : null;
+  };
+
+  const renderUrlToQRCode = (fieldName, url) => {
+    return url ? (
+      <div className={style.mt6}>
+        <Typography variant="h3" className={style.extraInfoFieldName}>
+          {fieldName}
+        </Typography>
+        <div className={style.extraInfoLabelWrapper}>
+          <QRCodeSVG value={url} marginSize={2} size={200} />
+        </div>
+      </div>
+    ) : null;
+  };
+
+  const renderSelectorToLable = (fieldName, options) => {
+    return options && options.length > 0 ? (
+      <div className={style.mt6}>
+        <Typography variant="h3" className={style.extraInfoFieldName}>
+          {fieldName}
+        </Typography>
+        <div className={style.extraInfoLabelWrapper}>
+          {options.map((option) => {
+            return (
+              <span key={option.name} className={style.extraInfoLabel}>
+                {option.name}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    ) : null;
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100%",
+        background: backgroundColor,
+        borderTop: "1px solid var(--lineColor)",
+      }}
+    >
+      <div
+        className="flexCentercontainer"
+        style={{ flexGrow: 1, overflow: "auto", padding: "0 24px", flexDirection: "column" }}
+      >
+        {playList.length > 0 ? (
+          <video
+            key={playList[currentIndex]} // 使用 key 来确保每次更换视频时重新加载
+            src={playList[currentIndex]}
+            controls
+            autoPlay
+            onEnded={handleVideoEnd}
+            style={{
+              height: "calc(100% - 74px)",
+              borderRadius: "12px",
+              width: "100%",
+            }}
+          />
+        ) : (
+          <p>No videos available to play.</p>
+        )}
+        <div
+          className={style.btnControl}
+        >
+          <Button
+            style={{ color: "#fff" }}
+            variant="jelly"
+            color="rgb(31, 41, 55)"
+            size="small"
+            prefixIcon={<ArrowLeftOutlined currentColor />}
+            onClick={handlePrevVideo}
+          >
+            上一个
+          </Button>
+          <Button
+            style={{ color: "#fff" }}
+            variant="jelly"
+            color="rgb(31, 41, 55)"
+            size="small"
+            suffixIcon={<ArrowRightOutlined currentColor />}
+            onClick={handleNextVideo}
+          >
+            下一个
+          </Button>
+        </div>
+      </div>
+      {showExtraInfo && (
+        <div
+          style={{
+            width: `${extraInfoWidth}%`,
+            overflow: "auto",
+            padding: "20px 24px",
+            backgroundColor: "rgb(17, 24, 39)",
+          }}
+        >
+          {records.length > 0 &&
+            fields.length > 0 &&
+            fields.map((field) => {
+              if (field.id !== videoFieldId) {
+                if (
+                  [FieldType.SingleSelect, FieldType.MultiSelect].includes(
+                    field.type
+                  )
+                ) {
+                  return renderSelectorToLable(
+                    field.name,
+                    records[currentIndex].getCellValue(field.id)
+                  );
+                } else if (field.type === FieldType.URL) {
+                  return renderUrlToQRCode(
+                    field.name,
+                    records[currentIndex].getCellValueString(field.id)
+                  );
+                } else {
+                  return renderText(
+                    field.name,
+                    records[currentIndex].getCellValueString(field.id)
+                  );
+                }
+              }
+            })}
+        </div>
+      )}
+      <Setting />
+    </div>
+  );
+};
+
+initializeWidget(VideoPlaylistPlayer, process.env.WIDGET_PACKAGE_ID);
---
+++ src/setting.jsx
@@ -0,0 +1,201 @@
+import React, { useState } from "react";
+import {
+  useSettingsButton,
+  ViewPicker,
+  useViewIds,
+  useCloudStorage,
+  useFields,
+  FieldPicker,
+  FieldType,
+} from "@apitable/widget-sdk";
+import { Checkbox, TextInput, Typography, Select } from "@apitable/components";
+import { PercentOutlined } from "@apitable/icons";
+import { SketchPicker } from "react-color";
+import style from "./index.css";
+
+export const Setting = () => {
+  const [isSettingOpened] = useSettingsButton();
+  const viewIds = useViewIds();
+  const [viewId, setViewId] = useCloudStorage("selectViewId", viewIds[0]);
+  const fields = useFields(viewId || viewIds[0]);
+  const [fieldId, setFieldId] = useCloudStorage("videoFieldId", fields[0].id);
+  const [showExtraInfo, setShowExtraInfo] = useCloudStorage(
+    "showExtraInfo",
+    false
+  );
+  const [extraInfoWidth, setExtraInfoWidth] = useCloudStorage(
+    "extraInfoWidth",
+    "20"
+  );
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+  const [backgroundColor, setBackgroundColor] = useCloudStorage(
+    "backgroundColor",
+    "#7B67EE"
+  );
+  const [carouselMode, setCarouselMode] = useCloudStorage("carouselMode", "list");
+  const [playListMode, setPlayListMode] = useCloudStorage("playListMode", "all");
+
+  return isSettingOpened ? (
+    <div
+      style={{
+        flexShrink: 0,
+        width: "300px",
+        borderLeft: "solid 1px var(--lineColor)",
+        paddingLeft: "16px",
+        paddingTop: "40px",
+        paddingRight: "16px",
+        background: "var(--defaultBg)",
+        overflowY: "auto",
+      }}
+    >
+      <h3 style={{ color: "var(--firstLevelText)" }}>配置</h3>
+      <div style={{ marginTop: "16px" }}>
+        <Typography variant="body2" color={"var(--thirdLevelText)"}>
+          通过视图获取播放清单
+        </Typography>
+        <div>
+          <ViewPicker
+            id="viewPicker"
+            viewId={viewId}
+            onChange={(option) => setViewId(option.value)}
+          />
+        </div>
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <Typography variant="body2" color={"var(--thirdLevelText)"}>
+          从播放清单中挑选
+        </Typography>
+        <div>
+          <Select
+            options={[
+              {
+                label: "播放全部",
+                value: "all",
+              },
+              {
+                label: "第一个视频",
+                value: "firstItem",
+              },
+              {
+                label: "最后一个视频",
+                value: "lastItem",
+              }
+            ]}
+            value={playListMode}
+            onSelected={(option) => {
+              setPlayListMode(option.value);
+            }}
+          />
+        </div>
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <Typography variant="body2" color={"var(--thirdLevelText)"}>
+          从哪个附件字段读取视频
+        </Typography>
+        <div>
+          <FieldPicker
+            id="FieldPicker"
+            viewId={viewId}
+            fieldId={fieldId}
+            onChange={(option) => setFieldId(option.value)}
+            allowedTypes={[FieldType.Attachment, FieldType.URL]}
+          />
+        </div>
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <Typography variant="body2" color={"var(--thirdLevelText)"}>
+          播放模式
+        </Typography>
+        <div>
+          <Select
+            options={[
+              {
+                label: "列表循环",
+                value: "list",
+              },
+              {
+                label: "单个循环",
+                value: "single",
+              },
+              {
+                label: "不循环",
+                value: "none",
+              }
+            ]}
+            value={carouselMode}
+            onSelected={(option) => {
+              setCarouselMode(option.value);
+            }}
+          />
+        </div>
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <Typography variant="body2" color={"var(--thirdLevelText)"}>
+          背景颜色
+        </Typography>
+        <div>
+          <div
+            className={style.swatch}
+            onClick={() => setDisplayColorPicker(!displayColorPicker)}
+          >
+            <div
+              style={{
+                backgroundColor: backgroundColor,
+                width: "36px",
+                height: "14px",
+                borderRadius: "2px",
+              }}
+            />
+          </div>
+          {displayColorPicker && (
+            <div className={style.colorPickerPopover}>
+              <div
+                className={style.colorPickerCover}
+                onClick={() => setDisplayColorPicker(false)}
+              />
+              <SketchPicker
+                color={backgroundColor}
+                onChange={(color) => {
+                  setBackgroundColor(color.hex);
+                }}
+                disableAlpha={true}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <Typography variant="body2" color={"var(--thirdLevelText)"}>
+          显示更多信息
+        </Typography>
+        <div>
+          <Checkbox
+            checked={showExtraInfo}
+            onChange={(value) => setShowExtraInfo(value)}
+          >
+            视频右侧显示文本信息
+          </Checkbox>
+          {showExtraInfo && (
+            <div>
+              <div className={style.row}>
+                <Typography variant="body4" color={"var(--thirdLevelText)"}>
+                  文本区域宽度
+                </Typography>
+                <TextInput
+                  placeholder="宽度百分比"
+                  size="small"
+                  value={extraInfoWidth}
+                  suffix={<PercentOutlined />}
+                  type="number"
+                  min={10}
+                  max={90}
+                  onChange={(e) => setExtraInfoWidth(e.target.value)}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  ) : null;
+};
